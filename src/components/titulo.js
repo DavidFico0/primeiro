cc2fe7d908d0f795638cc34e418c227c46a751ec
cloudsystems22 <cloudsystems22@gmail.com>
2020-06-01 15:30:30 -0300

    projeto react

---
+++ src/components/titulo.js
@@ -0,0 +1,12 @@
+//forma de importar componente, ref a let comp = require('../pasta')
+import React from 'react';
+
+function Titulo(){
+    return (
+        <p>
+            Eu sou componente!
+        </p>
+    )
+}
+
+export default Titulo;
